5d3e300f530790ca6f9a2b3c6f9891401e24c4d9
ltwebb <loritwebb@gmail.com>
2023-05-01 08:20:53 -0400

    Minor style tweaks

--- resources/views/app/todos/index.blade.php
+++ resources/views/app/todos/index.blade.php
@@ -1,13 +1,9 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Todo List') }}
-        </h2>
-    </x-slot>
+
 
     <div class="py-12">
         <div class="lg:w-2/3 max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white pt-4 overflow-hidden shadow-xl sm:rounded-lg">
                 <livewire:todo-list />
             </div>
         </div>
